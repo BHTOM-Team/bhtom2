72869e16ae74d1ed1c585daf123f0ff72f804092
Maja Jabłońska <majajjablonska@gmail.com>
2021-08-11 18:57:11 +0200

    add harvesters

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ docker-compose>1.25.0
 In the Docker directory:
 
 ``COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up -d --build``
+
+# Testing
+
+To run the tests provide your local database data in the ``.bhtom.env`` file.
+
+## Troubleshooting
+
+Make sure that the ``bhtom`` user has the permission to create the test database. You might need to alter user in your local DB:
+
+``ALTER USER bhtom CREATEDB;``
---
+++ tests/utils/observation_data_extra_data_utils_test.py
@@ -0,0 +1,39 @@
+from django.test import SimpleTestCase, TestCase
+
+import json
+from typing import Any, Dict, Optional, List
+
+from astropy.io import ascii
+from tom_dataproducts.exceptions import InvalidFileFormatException
+from tom_dataproducts.models import DataProduct
+
+from bhtom2.utils.observation_data_extra_data_utils import ObservationDatapointExtraData, decode_datapoint_extra_data
+
+
+class DecodeDictExtraDataTestCase(SimpleTestCase):
+    def test_decode_complete_extra_data(self):
+        comment_dict: Dict[str, str] = {
+            'facility': 'FacilityName',
+            'observation_time': '01-01-2021',
+            'owner': 'OwnerName'
+        }
+
+        observation_data_extra: ObservationDatapointExtraData = decode_datapoint_extra_data(comment_dict)
+
+        self.assertEqual(observation_data_extra.facility_name, 'FacilityName')
+        self.assertEqual(observation_data_extra.observation_time, '01-01-2021')
+        self.assertEqual(observation_data_extra.owner, 'OwnerName')
+        self.assertEqual(observation_data_extra.to_json_str(), '{"facility": "FacilityName", "observation_time": '
+                                                               '"01-01-2021", "owner": "OwnerName"}')
+
+    def test_decode_incomplete_extra_data(self):
+        comment_dict: Dict[str, str] = {
+            'facility': 'FacilityName',
+        }
+
+        observation_data_extra: ObservationDatapointExtraData = decode_datapoint_extra_data(comment_dict)
+
+        self.assertEqual(observation_data_extra.facility_name, 'FacilityName')
+        self.assertEqual(observation_data_extra.observation_time, None)
+        self.assertEqual(observation_data_extra.owner, None)
+        self.assertEqual(observation_data_extra.to_json_str(), '{"facility": "FacilityName"}')
