1ffcda0821bb1a1e530aa414d33f311c7671a346
MilenaRtj <92859700+MilenaRtj@users.noreply.github.com>
2023-01-16 14:40:55 +0100

    moletai obs

---
+++ bhtom2/bhtom_observations/facilities/moletai.py
@@ -0,0 +1,67 @@
+from crispy_forms.layout import Div
+from django import forms
+
+from bhtom_base.bhtom_observations.facility import BaseManualObservationFacility, BaseManualObservationForm
+
+SUCCESSFUL_OBSERVING_STATES = ['COMPLETED']
+FAILED_OBSERVING_STATES = ['WINDOW_EXPIRED', 'CANCELED', 'FAILURE_LIMIT_REACHED', 'NOT_ATTEMPTED']
+TERMINAL_OBSERVING_STATES = SUCCESSFUL_OBSERVING_STATES + FAILED_OBSERVING_STATES
+
+
+class OstrowikPhotometryObservationForm(BaseManualObservationForm):
+    name = forms.CharField()
+    start = forms.CharField(widget=forms.TextInput(attrs={'type': 'date'}))
+    end = forms.CharField(required=False, widget=forms.TextInput(attrs={'type': 'date'}))
+    observation_id = forms.CharField(required=False)
+    observation_params = forms.CharField(required=False, widget=forms.Textarea(attrs={'type': 'json'}))
+
+    def layout(self):
+        return Div(
+            Div('name', 'observation_id'),
+            Div(
+                Div('start', css_class='col'),
+                Div('end', css_class='col'),
+                css_class='form-row'
+            ),
+            Div('observation_params')
+        )
+
+class Ostrowik(BaseManualObservationFacility):
+    name = 'Ostrowik'
+    SITES = {
+        'Ostrowik': {
+            'sitecode': 'ostrowik',
+            'latitude': 21.42,
+            'longitude': 52.0897,
+            'elevation': 200
+        }
+    }
+    observation_forms = {
+        'PHOTOMETRIC_SEQUENCE': OstrowikPhotometryObservationForm,
+    }
+
+    def get_form(self, observation_type):
+        return self.observation_forms['PHOTOMETRIC_SEQUENCE']
+
+    def submit_observation(self, observation_payload):
+        # TODO: send mail
+        return []
+
+    def validate_observation(self, observation_payload):
+        # TODO: ?
+        return []
+
+    def get_terminal_observing_states(self):
+        return TERMINAL_OBSERVING_STATES
+
+    def get_observing_sites(self):
+        return self.SITES
+
+    def cancel_observation(self, observation_id):
+        return []
+
+    def get_observation_url(self, observation_id):
+        return ''
+
+    def all_data_products(self, observation_record):
+        return []
