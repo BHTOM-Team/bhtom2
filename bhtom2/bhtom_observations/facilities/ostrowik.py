befa835926068bc89432bb4313970bc27308c313
Maja Jabłońska <majajjablonska@gmail.com>
2023-01-16 15:47:15 +0100

    commented suspicious if in datamanagement

--- bhtom2/bhtom_observations/facilities/ostrowik.py
+++ bhtom2/bhtom_observations/facilities/ostrowik.py
@@ -1,19 +1,27 @@
-from crispy_forms.layout import Div
+from crispy_forms.layout import Div, Field
 from django import forms
+from crispy_forms.layout import Column, Div, HTML, Layout, Row, MultiWidgetField, Fieldset
 
 from bhtom_base.bhtom_observations.facility import BaseManualObservationFacility, BaseManualObservationForm
+from bhtom_base.bhtom_observations.widgets import FilterField
 
 SUCCESSFUL_OBSERVING_STATES = ['COMPLETED']
 FAILED_OBSERVING_STATES = ['WINDOW_EXPIRED', 'CANCELED', 'FAILURE_LIMIT_REACHED', 'NOT_ATTEMPTED']
 TERMINAL_OBSERVING_STATES = SUCCESSFUL_OBSERVING_STATES + FAILED_OBSERVING_STATES
 
 
-class OstrowikPhotometryObservationForm(BaseManualObservationForm):
+class OstrowikPhotometricSequenceObservationForm(BaseManualObservationForm):
+
     name = forms.CharField()
     start = forms.CharField(widget=forms.TextInput(attrs={'type': 'date'}))
     end = forms.CharField(required=False, widget=forms.TextInput(attrs={'type': 'date'}))
     observation_id = forms.CharField(required=False)
     observation_params = forms.CharField(required=False, widget=forms.Textarea(attrs={'type': 'json'}))
+    cadence_frequency = forms.IntegerField(required=True, help_text='in hours')
+    exposure_time = forms.FloatField(required=True, help_text='in seconds')
+
+    valid_filters = [['V', 'V'], ['R', 'R'], ['I', 'I'], ['Clear', 'CLEAR']]
+    filters = forms.ChoiceField(required=True, label='Filters', choices=valid_filters)
 
     def layout(self):
         return Div(
@@ -23,8 +31,10 @@ class OstrowikPhotometryObservationForm(BaseManualObservationForm):
                 Div('end', css_class='col'),
                 css_class='form-row'
             ),
+            Div('filters'),
             Div('observation_params')
         )
+
 
 class Ostrowik(BaseManualObservationFacility):
     name = 'Ostrowik'
@@ -37,7 +47,7 @@ class Ostrowik(BaseManualObservationFacility):
         }
     }
     observation_forms = {
-        'PHOTOMETRIC_SEQUENCE': OstrowikPhotometryObservationForm,
+        'PHOTOMETRIC_SEQUENCE': OstrowikPhotometricSequenceObservationForm,
     }
 
     def get_form(self, observation_type):
