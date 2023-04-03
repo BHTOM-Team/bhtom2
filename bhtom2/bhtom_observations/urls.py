bdb79a86632e295e49dac54ffdcd55026a673cb4
Maja Jablonska <majajjablonska@gmail.com>
2023-04-03 14:13:49 +0200

    Add Proposals details view

--- bhtom2/bhtom_observations/urls.py
+++ bhtom2/bhtom_observations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProposalListView
+from .views import ProposalListView, ProposalDetailView
 
 app_name = 'bhtom2.bhtom_observations'
 
 urlpatterns = [
     path('', ProposalListView.as_view(), name='list'),
+    path('<int:pk>/', ProposalDetailView.as_view(), name='detail'),
 ]
--- bhtom2/bhtom_observations/views.py
+++ bhtom2/bhtom_observations/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.http import HttpResponseForbidden
+from django.views.generic import ListView, DetailView
 from guardian.shortcuts import get_objects_for_user
 
 from bhtom_base.bhtom_observations.models import Proposal
@@ -15,3 +16,18 @@ class ProposalListView(LoginRequiredMixin, ListView):
             return Proposal.objects.all()
         else:
             return get_objects_for_user(self.request.user, Proposal)
+
+
+class ProposalDetailView(DetailView):
+    template_name = 'bhtom_observations/proposal_detail.html'
+    strict = False
+    model = Proposal
+
+    def get_object(self, queryset=None):
+        obj = super(ProposalDetailView, self).get_object(queryset=queryset)
+        if self.request.user.has_perm('bhtom_observations.view_proposal'):
+            return obj
+        elif self.request.user in obj.users:
+            raise obj
+        else:
+            return HttpResponseForbidden()
