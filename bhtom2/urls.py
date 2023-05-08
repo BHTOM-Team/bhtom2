5a349419d7df2a4fc379893dcaee135b14391121
arturkrawczyk <akrawczyk@akond.com>
2023-05-08 17:15:37 +0200

    #51

--- bhtom2/urls.py
+++ bhtom2/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', include('bhtom_custom_registration.bhtom_registration.registration_flows.approval_required.urls', namespace='registration')),
     path('alerts/query/list/', BrokerQueryListView.as_view(template_name='bhtom_alerts/brokerquery_list.html'), name='alerts:list'),
     path('targets/', TargetListView.as_view(), name='targets'),
-    path('targets/<int:pk>/', TargetDetailView.as_view(template_name='bhtom_targets/target_detail.html'), name='detail'),
+    path('targets/<str:name>/', TargetDetailView.as_view(template_name='bhtom_targets/target_detail.html'), name='detail'),
     path('targets/create/', TargetCreateView.as_view(template_name='bhtom_targets/target_form.html'), name='create'),
     path('targets/<int:pk>/update/', TargetUpdateView.as_view(template_name='bhtom_targets/target_form.html'), name='update'),
     path('targets/<int:pk>/download-photometry', TargetDownloadPhotometryDataView.as_view(), name='download_photometry_data'),
