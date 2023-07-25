1591c6d2e55679064a3cae47a60d71b858ff74fe
arturkrawczyk <akrawczyk@akond.com>
2023-07-25 18:28:41 +0200

    add kafka

--- bhtom2/bhtom_calibration/models.py
+++ bhtom2/bhtom_calibration/models.py
@@ -2,20 +2,28 @@ from django.db import models
 from bhtom_base.bhtom_dataproducts.models import DataProduct
 
 
-class Catalogs(models.Model):
+class catalogs(models.Model):
     id = models.IntegerField(primary_key=True)
     survey = models.TextField(blank=False, editable=False)
     filters = models.TextField(blank=False, editable=False)
     isActive = models.BooleanField(default=True)
 
 
-class cpcsData(models.Model):
+class calibration_data(models.Model):
+
+    STATUS = [
+        ('C', 'TO DO'),
+        ('P', 'IN PROGRESS'),
+        ('S', 'SUCCESS'),
+        ('E', 'ERROR')
+    ]
 
     id = models.AutoField(db_index=True, primary_key=True)
-    dataproduct_id = models.ForeignKey(DataProduct, on_delete=models.CASCADE)
+    dataproduct = models.ForeignKey(DataProduct, on_delete=models.CASCADE)
+    use_catalog = models.ForeignKey(catalogs, on_delete=models.CASCADE, null=True)
+    status = models.CharField(max_length=1, choices=STATUS, default='C')
     status_message = models.TextField(null=True, blank=True)
     mjd = models.FloatField(null=True, blank=True)
-    expTime = models.FloatField(null=True, blank=True)
     mag = models.FloatField(null=True, blank=True)
     mag_error = models.FloatField(null=True, blank=True)
     ra = models.FloatField(null=True, blank=True)
@@ -24,16 +32,16 @@ class cpcsData(models.Model):
     outlier_fraction = models.FloatField(null=True, blank=True)
     scatter = models.FloatField(null=True, blank=True)
     npoints = models.IntegerField(null=True, blank=True)
-    processing_time = models.DateTimeField(null=True, blank=True, editable=False)
+    processing_time = models.FloatField(null=True, blank=True)
     created = models.DateTimeField(null=True, blank=True, editable=False)
     modified = models.DateTimeField(null=True, blank=True, editable=True)
     best_filter = models.CharField(max_length=5, null=True, blank=True)
     match_distans = models.FloatField(default=0.5)
-    useCatalog = models.ForeignKey(Catalogs, on_delete=models.CASCADE)
-    noPlot = models.BooleanField(default=True)
-    cpcsPlot = models.TextField(null=True, blank=True)
+    no_plot = models.BooleanField(default=True)
+    calibration_plot = models.TextField(null=True, blank=True)
     comment = models.TextField(null=True, blank=True)
+    number_tries = models.IntegerField(null=False, default=0)
 
     class Meta:
         verbose_name = 'cpcs processing file'
-        verbose_name_plural = "cpcsData"
+        verbose_name_plural = "calibration_data"
