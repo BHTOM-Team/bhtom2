98dc7d8e0bf5bce58ff1b124bd94c2d3b34f8db0
Yurii Purdenko <yurii.purddenko@gmail.com>
2023-10-02 10:08:55 +0200

    fix script to upload

--- Documentation/documentation_scripts/upload_files_script.py
+++ Documentation/documentation_scripts/upload_files_script.py
@@ -1,48 +1,35 @@
-import requests
-import argparse
 import os
+import requests
 
-def upload_files(token, target, data_product_type, files, match_dist=None, comment=None, dry_run=False, no_plot=False, mjd=None, group=None, observer=None):
-    headers = {
-        'Authorization': f'Token {token}'
-    }
+# The directory containing all the necessary fits files
+directory = '### DIRECTORY NAME ###'
 
-    data = {
-        'target': target,
-        'data_product_type': data_product_type,
-        'match_dist': match_dist,
-        'comment': comment,
-        'dry_run': dry_run,
-        'no_plot': no_plot,
-        'mjd': mjd,
-        'group': group,
-        'observer': observer
-    }
-    "url to upload service"
-    api_url=
+token= '### Yours Token ###'
+target_name = '### TARGET NAME ###'
+observatory_name= '### Observatory name ###'
+filter_name = '### FILTER NAME ###'
 
-    response = requests.post(api_url, headers=headers, json=data, files=files)
+data_product_type = 'fits_file'
 
-    print(response.json())
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Upload files to BHTOM2 API")
-    parser.add_argument("token", type=str, help="Authentication token")
-    parser.add_argument("target", type=str, help="Target of the uploaded files")
-    parser.add_argument("data_product_type", type=str, help="Type of data product")
-    parser.add_argument("files", type=str, help="Comma-separated list of files to upload")
-    parser.add_argument("--match_dist", type=float, help="Matching distance")
-    parser.add_argument("--comment", type=str, help="Additional comment")
-    parser.add_argument("--dry_run", action="store_true", help="Dry run mode (true or false)")
-    parser.add_argument("--no_plot", action="store_true", help="Disable plot generation (true or false)")
-    parser.add_argument("--mjd", type=float, help="Modified Julian Date")
-    parser.add_argument("--group", type=str, help="Group associated with the files")
-    parser.add_argument("--observer", type=str, help="Observer's name")
-
-    args = parser.parse_args()
-
-    # Split the comma-separated list of files into a list
-    files_list = [f.strip() for f in args.files.split(",")]
-
-    upload_files(args.token, args.target, args.data_product_type, files_list,
-                 args.match_dist, args.comment, args.dry_run, args.no_plot, args.mjd, args.group, args.observer)
+# Dry run option should be set to "True" or "False" (as a string)
+dry_run = '### DRY RUN ###'
+comment = '### comment ###'
+for filename in os.listdir(directory):
+    with open(os.path.join(directory, filename), 'rb') as f:
+        print("Sending...")
+        response = requests.post(
+            url='https://uploadsvc2.astrolabs.pl/upload/',
+            headers={
+                'Authorization': "Token " + str(token)
+            },
+            data={
+                'target': target_name,
+                'filter': filter_name,
+                'data_product_type': data_product_type,
+                'dry_run': dry_run,
+                'observatory': observatory_name,
+                'comment': comment,
+            },
+            files={'files': f}
+        )
+        print(response.json())
