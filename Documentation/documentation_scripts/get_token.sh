290eabc03dd72668726dedc3a4ee9fce2777d12a
Przemek Mikołajczyk <przeminio@gmail.com>
2023-11-09 11:59:25 +0100

    Update get_token.sh

--- Documentation/documentation_scripts/get_token.sh
+++ Documentation/documentation_scripts/get_token.sh
@@ -4,6 +4,6 @@ curl -X 'POST' \
   -H 'Content-Type: application/json' \
   -H 'X-CSRFToken: uUz2fRnXhPuvD9YuuiDW9cD1LsajeaQnE4hwtEAfR00SgV9bD5HCe5i8n4m4KcOr' \
   -d '{
-  "username": "mikolajczyk",
-  "password": "Quazarek28!"
+  "username": login
+  "password": password
 }'
