3dfd4e8dfa5b4ca750155faf19835ddf4fcae359
Yurii Purdenko <yurii.purddenko@gmail.com>
2023-12-06 12:21:03 +0100

    fix: date

--- bhtom2/settings.py
+++ bhtom2/settings.py
@@ -226,7 +226,7 @@ TOM_REGISTRATION = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -234,7 +234,7 @@ USE_L10N = False
 
 USE_TZ = True
 
-DATETIME_FORMAT = 'Y-m-d H:m:s'
+DATETIME_FORMAT = 'Y-m-d H:i:s'
 DATE_FORMAT = 'Y-m-d'
 
 # Static files (CSS, JavaScript, Images)
