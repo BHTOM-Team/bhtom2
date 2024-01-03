30d2aa11703fe7bc32420869652bf3a457beacfd
Yurii Purdenko <yurii.purddenko@gmail.com>
2024-01-03 15:39:40 +0100

    fix: move harvesters to harvester-service

--- bhtom2/bhtom_catalogs/views.py
+++ bhtom2/bhtom_catalogs/views.py
@@ -2,6 +2,9 @@ from django.views.generic.edit import FormView
 from django.urls import reverse
 from urllib.parse import urlencode
 from django.core.exceptions import ValidationError
+import requests
+from django.conf import settings
+import json
 
 from .forms import CatalogQueryForm
 from bhtom_base.bhtom_catalogs.harvester import MissingDataException
@@ -26,13 +29,28 @@ class CatalogQueryView(FormView):
         :type form: CatalogQueryForm
         """
         try:
-            # harvester might return a dictionary of extras
-            self.target, self.ex = form.get_target()
-        #            print("extras from harvester: ",self.ex)
-
+            term = form.cleaned_data['term']
+            service = form.cleaned_data['service']
+            post_data = {
+                'terms': term,
+                'harvester': service
+            }
+            response = requests.post(settings.HARVESTER_URL + 'findTargetWithHarvester/', data=post_data)
+            
+            if response.status_code == 200:
+                # Extract JSON from the response
+                self.target = json.loads(response.text)
+            else:
+                response.raise_for_status()
         except MissingDataException:
             form.add_error('term', ValidationError('Object not found'))
             return self.form_invalid(form)
+        except Exception as e:
+            # Handle other exceptions, log the error, etc.
+            form.add_error('term', ValidationError('Object not found'))
+            logger.error("Oops something went wrong: " + str(e))
+            return self.form_invalid(form)
+
         return super().form_valid(form)
 
     def get_success_url(self):
@@ -40,6 +58,5 @@ class CatalogQueryView(FormView):
         Redirects to the ``TargetCreateView``. Appends the target parameters to the URL as query parameters in order to
         autofill the ``TargetCreateForm``, including any additional names returned from the query.
         """
-        target_params = self.target.as_dict()
-        target_params['names'] = ','.join(getattr(self.target, 'extra_names', []))
-        return reverse('targets:create') + '?' + urlencode(target_params) + '&' + urlencode(self.ex)
+        return reverse('targets:create') + '?' + urlencode(self.target)
+
