bdcb1029211dfb4b3065ade7806653d37b336914
arturkrawczyk <akrawczyk@akond.com>
2024-03-06 17:43:58 +0100

    add spectroscopy model

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -3,24 +3,22 @@
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $SQL_HOST $SQL_PORT;do
       sleep 0.1
     done
-
     echo "PostgreSQL started"
 fi
 
 echo "Collecting static..."
 
-while ! python3 manage.py collectstatic --no-input 2>&1; do
+while ! python3 manage.py collectstatic --no-input 2>&1;do
   echo "Collecting static..."
   sleep 3
 done
 
 echo "Migrating..."
 
-while ! python3 manage.py migrate 2>&1; do
+while ! python3 manage.py migrate 2>&1;do
    echo "Migration is in progress..."
    sleep 3
 done
