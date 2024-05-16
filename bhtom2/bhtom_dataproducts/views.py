a01676e1bb07960e8f806bebd7de3695f880b7be
Lukasz Wyrzykowski <wyrzykow@gmail.com>
2024-05-16 17:31:16 +0200

    matchdist default from 0.5 to 2

--- bhtom2/bhtom_dataproducts/views.py
+++ bhtom2/bhtom_dataproducts/views.py
@@ -77,7 +77,7 @@ class DataProductUploadView(LoginRequiredMixin, FormView):
         camera = form.cleaned_data['camera']
         observation_filter = form.cleaned_data['filter']
         mjd = form.cleaned_data['mjd']
-        match_dist = 0.5
+        match_dist = 2.0
         dry_run = form.cleaned_data['dryRun']
         comment = form.cleaned_data['comment']
         observer = form.cleaned_data['observer']
