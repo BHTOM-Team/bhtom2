87c6b38d7e6f3896138b52bd6ca9337138b566ce
Yurii Purdenko <yurii.purddenko@gmail.com>
2024-08-05 14:53:55 +0200

    fix

--- bhtom2/utils/api_pagination.py
+++ bhtom2/utils/api_pagination.py
@@ -3,7 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
-    max_page_size = 1
+    max_page_size = 1000
 
     def get_paginated_response(self, data):
         return Response({
