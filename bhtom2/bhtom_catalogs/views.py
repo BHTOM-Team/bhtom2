81cdd2301890553258816dff1e9ccfcd3c441084
Lukasz Wyrzykowski <wyrzykow@gmail.com>
2024-11-28 23:18:23 +0100

    fix(catalogs): temp extended error messaging in CatalogQueryView

--- bhtom2/bhtom_catalogs/views.py
+++ bhtom2/bhtom_catalogs/views.py
@@ -51,7 +51,9 @@ class CatalogQueryView(FormView):
             return self.form_invalid(form)
         except Exception as e:
             # Handle other exceptions, log the error, etc.
-            form.add_error('term', ValidationError('Object not found'))
+#            form.add_error('term', ValidationError(f'Object not found'))
+            sanitized_message = f"Object not found: {str(e)}"
+            form.add_error('term', ValidationError(sanitized_message))
             logger.error("Oops something went wrong: " + str(e))
             return self.form_invalid(form)
 
